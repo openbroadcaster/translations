08d43df8d4565923f407d76c98f655bea2a95ca6
Ruby Wolf <ruby.w@pm.me>
2019-08-09 21:45:37 +0000

    Initial commit.

---
+++ module.php
@@ -0,0 +1,18 @@
+<?php
+
+class Translations extends OBFModule {
+  public $name = 'Translations';
+  public $description = 'Add multi-language support to OpenBroadcaster.';
+
+  public function callbacks () {
+
+  }
+
+  public function install () {
+    return true;
+  }
+
+  public function uninstall () {
+    return true;
+  }
+}
