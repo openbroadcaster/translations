d22c7590cfa511eda6631db39354dccbaa668b8f
Ruby Wolf <ruby.w@pm.me>
2019-08-21 22:36:25 +0000

    Added DB tables for translation languages and values.

--- module.php
+++ module.php
@@ -21,6 +21,21 @@ class TranslationsModule extends OBFModule {
       PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
 
+    $this->db->query('CREATE TABLE IF NOT EXISTS `module_translations_languages` (
+      `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+      `name` varchar(255) NOT NULL,
+      `code` varchar(50) NOT NULL UNIQUE,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
+    $this->db->query('CREATE TABLE IF NOT EXISTS `module_translations_values` (
+      `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+      `language_id` int(10) unsigned NOT NULL,
+      `source_str` text NOT NULL,
+      `result_str` text NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     return true;
   }
 
